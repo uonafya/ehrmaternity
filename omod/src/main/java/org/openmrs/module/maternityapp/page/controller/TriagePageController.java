8babb2e2ca7143231808760b512922fe0ae59f77
franqq <githae.francis@gmail.com>
2016-08-31 08:35:09 +0300

    Save triage information

--- omod/src/main/java/org/openmrs/module/maternityapp/page/controller/TriagePageController.java
+++ omod/src/main/java/org/openmrs/module/maternityapp/page/controller/TriagePageController.java
@@ -1,13 +1,18 @@
 package org.openmrs.module.maternityapp.page.controller;
 
 import org.apache.commons.lang.StringUtils;
+import org.openmrs.Encounter;
 import org.openmrs.Patient;
 import org.openmrs.PatientProgram;
 import org.openmrs.Program;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
+import org.openmrs.module.hospitalcore.PatientQueueService;
+import org.openmrs.module.hospitalcore.model.OpdPatientQueue;
 import org.openmrs.module.hospitalcore.model.PatientSearch;
+import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.ui.framework.page.PageRequest;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Calendar;
@@ -41,5 +46,25 @@ public class TriagePageController {
         model.addAttribute("patientId", patient.getPatientId());
         model.addAttribute("title","Maternity Triage");
 
+    }
+
+    public String post(
+            @RequestParam("patientId") Patient patient,
+            @RequestParam("queueId") Integer queueId,
+            PageRequest request,
+            UiSessionContext session,
+            UiUtils ui) {
+        try {
+            OpdPatientQueue patientQueue = Context.getService(PatientQueueService.class).getOpdPatientQueueById(queueId);
+            ClinicalForm form = ClinicalForm.generateForm(request.getRequest(), patient, null);
+            //Set this based on count of previous FP encounters
+            String encounterType = FamilyPlanningMetadata._FamilyPlanningEncounterType.FP_NEW_ENCOUNTER_TYPE;
+            Encounter encounter = Context.getService(MchService.class).saveMchEncounter(form, encounterType, session.getSessionLocation());
+            QueueLogs.logOpdPatient(patientQueue, encounter);
+        } catch (ParseException e) {
+            log.error(e.getMessage());
+            return null;
+        }
+        return "redirect:" + ui.pageLinkWithoutContextPath("patientqueueapp", "mchClinicQueue", null);
     }
 }
